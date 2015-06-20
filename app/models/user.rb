51a89a4524ab7590b9a7acad9055d1d5c4918644
Fukuo Kadota <app.fk91@gmail.com>
2015-06-20 17:59:59 +0900

    Add line

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ActiveRecord::Base
   def self.build_permissions(perms, other, target)
     perms.permits! :read
   end
+
   def build_permissions(perms, other)
     perms.permits! :read
     perms.permits! :write
