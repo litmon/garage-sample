cde585251380ebb7c8b4fedb4fd6c33510e006ed
Fukuo Kadota <app.fk91@gmail.com>
2015-06-28 23:49:16 +0900

    今日のcommit

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,9 @@ class User < ActiveRecord::Base
   include Garage::Representer
   include Garage::Authorizable
 
-  property :id
-  property :name
-  property :email
+  # property :id
+  # property :name
+  # property :email
 
   def self.build_permissions(perms, other, target)
     perms.permits! :read
